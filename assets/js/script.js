6c54e2415c226f7f87f6be4e80ebd98f3d3b2777
raimasaniyat <raimasaniyat@gmail.com>
2026-03-09 01:54:12 +0600

    Login functionality completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,18 @@
-console.log("hello JS!");
-
 document.getElementById("login-btn").addEventListener("click", function(){
-    console.log("login button clicked!");
+    
+    const nameInput = document.getElementById("userNameInput");
+    const userName = nameInput.value;
+
+    const passInput = document.getElementById("userPasswordInput");
+    const userPass = passInput.value;
+
+    if(userName == "admin" && userPass == "admin123"){
+        alert("login successful");
+        window.location.assign("./home.html");
+    }
+    else{
+        alert("error! give the correct input.");
+        return;
+    }
 })
+
